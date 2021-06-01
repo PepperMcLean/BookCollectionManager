d4da99d0ec50f3bda4a60f50b6511ed9fa9df088
PepperMcLean <jackmcl206@gmail.com>
2021-05-31 23:16:52 -0500

    CRUD working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,12 @@ class ApplicationController < Sinatra::Base
         set :session_secret, "n[K#!PF!c.E6w%0f-z(Cn7|&paOoE6hVrvXkzM=#V-Vp5ITt.fL2,cAF~;[yva;"
     end
 
-    get '/' do 
-        erb :welcome
+    get '/' do
+		if logged_in?
+			redirect "/home"
+		else
+			erb :welcome
+		end
     end
 
     get '/signup' do 
@@ -31,17 +35,63 @@ class ApplicationController < Sinatra::Base
 		user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect "/success"
+			redirect "/home"
 		else
 			redirect "/failure"
 		end
 	end
 
-    get "/success" do
+    get "/home" do
 		if logged_in?
-			erb :success
+			erb :home
 		else
-			redirect "/login"
+			redirect "/error"
+		end
+	end
+
+	get '/books/show' do
+		if logged_in?
+			@books = []
+			Book.all.each do |book|
+				if book.user_id == current_user.id
+					@books << book
+				end
+			end
+			erb :show
+		else
+			redirect "/error"
+		end
+	end
+
+	get '/books/new' do
+		if logged_in?
+			@book = Book.new
+			erb :new
+		else
+			redirect "/error"
+		end
+	end
+	
+	post '/books' do
+		@book = Book.create(name: params[:name], author: params[:author], genre: params[:genre], publication_date: params[:publication_date], personal_notes: params[:personal_notes], publication_date: params[:publication_date], user_id: current_user.id)
+		redirect to "/books/#{@book.id}"
+	end
+
+	get '/books/:id' do
+		@book = Book.find(params[:id])
+		if logged_in? and @book.user_id == current_user.id
+			erb :show_id
+		else
+			redirect "/error"
+		end
+	end
+
+	get '/books/:id/edit' do
+		@book = Book.find(params[:id])
+		if logged_in? and @book.user_id == current_user.id
+			erb :edit
+		else
+			redirect "/error"
 		end
 	end
 
@@ -54,6 +104,23 @@ class ApplicationController < Sinatra::Base
 		redirect "/"
 	end
 
+    get "/error" do
+        erb :error
+    end
+
+	patch '/books/:id' do
+		id = params[:id]
+		@book = Book.find_by(id: id)
+		attrs = params[:book]
+		@book.update(attrs)
+		redirect to "/books/#{@book.id}"
+	end
+
+	delete '/books/:id' do
+		Book.delete(params[:id])
+		redirect to "/home"
+	end
+	
     helpers do
 		def logged_in?
 			!!session[:user_id]
